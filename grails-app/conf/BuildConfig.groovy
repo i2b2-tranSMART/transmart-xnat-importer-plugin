3eabeac775a77139067a83a0004f3273251e63ff
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 17:02:54 -0500

    cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,8 +14,6 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		// runtime ':biomart-domain:16.2-SNAPSHOT'
-
 		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
 			export = false
 		}
